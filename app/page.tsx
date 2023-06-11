b4dba523a803a64e53cc3a4758ea9f27c0081c12
John Seong <wonmor@gmail.com>
2023-06-11 13:51:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,58 @@
-// Import necessary modules
 import React from 'react'
 
 export default function Portfolio() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white py-2 text-center">
-            <header className="text-4xl font-bold mt-10">
-                <h1 className="font-sans text-gray-800">John Seong</h1>
-                <p className="text-2xl text-gray-600 mt-2">Software Engineer</p>
-            </header>
+  return (
+    <div className="flex flex-col min-h-screen bg-white text-gray-800">
 
-            <section className="w-full px-10 mt-10">
-                <h2 className="text-3xl font-semibold text-gray-800 mb-6">Skills</h2>
-                <div className="flex flex-wrap justify-center items-center gap-10">
-                    <div className="flex flex-col items-center">
-                        <img src="/icons/swiftui.svg" alt="SwiftUI" className="w-16 h-16"/>
-                        <p className="mt-2 text-gray-700">SwiftUI</p>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <img src="/icons/python.svg" alt="Python" className="w-16 h-16"/>
-                        <p className="mt-2 text-gray-700">Python</p>
-                    </div>
+      {/* Header */}
+      <header className="p-6 bg-blue-500 text-white">
+        <h1 className="text-3xl font-bold">John Seong</h1>
+        <p className="text-lg">Software Engineer</p>
+      </header>
 
-                    {/* Add more skill icons here... */}
-                </div>
-            </section>
+      {/* Main content */}
+      <main className="flex-grow flex flex-col items-center justify-center p-6">
 
-            <h2 className="text-3xl font-semibold text-gray-800 mt-10">Projects</h2>
+        {/* About */}
+        <section className="p-6 max-w-2xl mb-12">
+          <h2 className="text-2xl font-bold mb-4">About Me</h2>
+          <p>I'm a Software Engineer specializing in front end development with an interest in back end technologies. Experienced in modern JavaScript technologies and libraries, and a diverse set of skills covering areas such as responsive web design, accessibility, and performance optimization.</p>
+        </section>
 
-            <main className="flex flex-wrap justify-around items-center w-full px-10">
-                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
-                    <div className="md:flex">
-                        <div className="md:flex-shrink-0">
-                            <img className="h-48 w-full object-cover md:w-48" src="/img/work1.jpg" alt="Project image"/>
-                        </div>
-                        <div className="p-8">
-                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Project 1</div>
-                            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Awesome Project</a>
-                            <p className="mt-2 text-gray-500">Description of the project...</p>
-                        </div>
-                    </div>
-                </div>
-                
-                {/* ... more project cards ... */}
-                
-            </main>
+        {/* Skills */}
+        <section className="p-6 max-w-2xl mb-12">
+          <h2 className="text-2xl font-bold mb-4">Skills</h2>
+          <div className="flex flex-wrap justify-center">
+            <div className="p-4">
+              <img src="/icons/swiftui.svg" alt="SwiftUI" className="w-12 h-12 mb-2"/>
+              <p>SwiftUI</p>
+            </div>
+            {/* Add more skills here */}
+          </div>
+        </section>
 
-            <footer className="flex items-center justify-center w-full h-24 border-t mt-10">
-                <a className="flex items-center justify-center">
-                    &copy; {new Date().getFullYear()} John Seong. All rights reserved.
-                </a>
-            </footer>
-        </div>
-    )
+        {/* Projects */}
+        <section className="p-6 max-w-2xl mb-12">
+          <h2 className="text-2xl font-bold mb-4">Projects</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="rounded overflow-hidden shadow-md">
+              <img src="/img/project.jpg" alt="Project" className="w-full"/>
+              <div className="px-6 py-4">
+                <h3 className="font-bold text-xl mb-2">Project Title</h3>
+                <p>Project description...</p>
+              </div>
+            </div>
+            {/* Add more project cards here */}
+          </div>
+        </section>
+
+      </main>
+
+      {/* Footer */}
+      <footer className="p-6 bg-blue-500 text-white text-center">
+        <p>&copy; {new Date().getFullYear()} John Seong. All rights reserved.</p>
+      </footer>
+
+    </div>
+  )
 }
