84e210d0a72db99aca0ee5fd27447756bcdf51c8
John Seong <wonmor@gmail.com>
2023-06-28 16:47:41 -0400

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import React from "react";
+import "./globals.css";
 
-import './globals.css';
+import { Roboto_Mono } from "next/font/google";
 
-import { Roboto_Mono } from 'next/font/google'
-
-const robotoMono = Roboto_Mono({ subsets: ['latin'] });
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'johnS',
-  description: 'Hi, I’m John. I’m a software engineer.',
+  title: "johnS",
+  description: "Hi, I’m John. I’m a software engineer.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className={[robotoMono.className, "flex flex-col min-h-screen bg-gray-800 text-white"].join(" ")}>
-      {/* Header */}
-      <header className="p-6 border-b border-gray-600 text-center">
-        <h1 className="text-6xl font-thin">john seong.</h1>
-      </header>
+    <html lang="en">
+      <div
+        className={[
+          robotoMono.className,
+          "flex flex-col min-h-screen bg-gray-800 text-white",
+        ].join(" ")}
+      >
+        {/* Header */}
+        <header className="p-6 border-b border-gray-600 text-center">
+          <h1 className="text-6xl font-thin">john seong.</h1>
+        </header>
 
-      {/* Main content */}
-      <body>{children}</body>
+        {/* Main content */}
+        <body>{children}</body>
 
-      {/* Footer */}
-      <footer className="p-6 border-t border-gray-600 font-light text-xl text-center">
-        <p>&copy; {new Date().getFullYear()} John Seong. All rights reserved.</p>
-      </footer>
-    </div>
+        {/* Footer */}
+        <footer className="p-6 border-t border-gray-600 font-light text-xl text-center">
+          <p>
+            &copy; {new Date().getFullYear()} John Seong. All rights reserved.
+          </p>
+        </footer>
+      </div>
+    </html>
   );
 }
