83e03caf7507a06fa2bd34de9d8222302c4a5f0e
John Seong <wonmor@gmail.com>
2024-07-06 22:08:37 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
- 
+
 const PUBLIC_FILE = /\.(.*)$/
- 
+
 export async function middleware(req: NextRequest) {
   if (
     req.nextUrl.pathname.startsWith('/_next') ||
@@ -10,10 +10,17 @@ export async function middleware(req: NextRequest) {
   ) {
     return
   }
- 
+
+  // Check for the hostname and redirect to the Korean locale if it's "johnseong.kr"
+  if (req.headers.get('host') === 'www.johnseong.kr') {
+    return NextResponse.redirect(
+      new URL(`/ko${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+    )
+  }
+
   if (req.nextUrl.locale === 'default') {
     const locale = req.cookies.get('NEXT_LOCALE')?.value || 'en'
- 
+
     return NextResponse.redirect(
       new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
     )
