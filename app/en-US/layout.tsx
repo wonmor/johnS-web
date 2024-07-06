cb3a869d85e71d97e07f6d1d256401f2dd7dfa8c
John Seong <wonmor@gmail.com>
2024-07-06 23:36:06 +0900

    Update layout.tsx

--- app/en-US/layout.tsx
+++ app/en-US/layout.tsx
@@ -3,6 +3,11 @@ import Script from "next/script";
 
 import { Outfit } from "next/font/google";
 
+import localFont from "next/font/local";
+
+const lightFont = localFont({ src: "../../public/GmarketSansLight.otf" });
+const mediumFont = localFont({ src: "../../public/GmarketSansMedium.otf" });
+
 import "../globals.css";
 
 const font = Outfit({
@@ -43,7 +48,10 @@ export default function RootLayout({
         ].join(" ")}
       >
         {/* Header */}
-        <header className="p-6 border-b border-gray-600 bg-gray-900 justify-center items-center text-center flex flex-col gap-4 mb-5">
+        <header  className={[
+          mediumFont.className,
+          "p-6 border-b border-gray-600 bg-gray-900 justify-center items-center text-center flex flex-col gap-4 mb-5",
+        ].join(" ")}>
           <div className="flex items-center justify-center flex-row gap-4 text-2xl">
             <Link href="https://johnseong.com">
               <span>English</span>
@@ -52,7 +60,7 @@ export default function RootLayout({
             <span>•</span>
 
             <Link href="https://johnseong.kr">
-              <span>Korean</span>
+              <span>한국어</span>
             </Link>
           </div>
         </header>
