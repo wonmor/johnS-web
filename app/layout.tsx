d080d0ca47b8c7793d593bf7e112e2e78d884e76
John Seong <wonmor@gmail.com>
2025-08-03 23:54:18 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,7 +111,7 @@ export default function RootLayout({
         </footer>
            {/* Header styled like London Underground roundel */}
         <header className="bg-[#e32017] text-white p-6 text-center flex flex-col items-center justify-center gap-1">
-          <h1 className="text-4xl tracking-widest uppercase">ORCHESTR INC.</h1>
+          <h1 className="text-4xl tracking-widest uppercase">ORCHESTR INC.™</h1>
           <div className="bg-white h-2 w-36 mt-2 mb-1"></div>
           <p className="text-sm uppercase tracking-wide">
             A DELAWARE COMPANY BASED IN LONDON
