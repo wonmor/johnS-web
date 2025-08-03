7ebb9d8851fc87f37ccd6943ab19c75997b2509d
John Seong <wonmor@gmail.com>
2025-08-03 21:53:14 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           <h1 className="text-4xl tracking-widest uppercase">ORCHESTR INC.</h1>
           <div className="bg-white h-2 w-36 mt-2 mb-1"></div>
           <p className="text-sm uppercase tracking-wide">
-            A DELAWARE COMPANY BASED IN LONDON
+            A DELAWARE COMPANY
           </p>
         </header>
 
