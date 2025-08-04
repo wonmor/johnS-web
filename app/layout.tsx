58d0ea74543d4db45c02dc7427290abb6ead9654
John Seong <wonmor@gmail.com>
2025-08-04 20:14:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,11 +112,18 @@ export default function RootLayout({
           </div>
         </footer>
 
+        <div className="flex justify-center mt-10 mb-6">
+          <h1 className="text-4xl tracking-widest uppercase">
+            COMPUTER
+            <br />
+            VISION. SORTED.
+          </h1>
+        </div>
         {/* Header styled like London Underground roundel */}
         <header className="bg-[#e32017] text-white p-6 text-center flex flex-col items-center justify-center gap-1">
-          <p>COMPUTER VISION. SORTED.</p>
+          <CityOfLondonLogo />
           <h1 className="text-4xl tracking-widest uppercase mt-2">
-            ORCHESTR<br />EUROPE LTD.
+            ORCHESTR INC.
           </h1>
           <div className="bg-white h-2 w-36 mt-2 mb-1"></div>
           <p className="text-sm uppercase tracking-wide">
