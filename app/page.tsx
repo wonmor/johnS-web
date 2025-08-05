192405c7f5e45e5e92300b54fb512263b35dca02
John Seong <wonmor@gmail.com>
2025-08-05 04:43:31 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -193,14 +193,20 @@ export default function Portfolio() {
             <p className="font-semibold">
               Apple WWDC23 Swift Student Challenge Winner
             </p>
+             <p className="flex items-center gap-1 mt-2">
+              <Image
+                src="/american-flag.png"
+                alt="American Flag"
+                width={24}
+                height={16}
+              />
+              <span>US O-1A Extraordinary Ability Visa</span>
+            </p>
             <p className="flex items-center gap-1 mt-2">
               <Image src="/uk-flag.png" alt="UK Flag" width={24} height={16} />
               <span>UK Global Talent Visa Holder</span>
             </p>
-               <p className="flex items-center gap-1 mt-2">
-              <Image src="/american-flag.png" alt="American Flag" width={24} height={16} />
-              <span>US O-1A Extraordinary Ability Visa</span>
-            </p>
+           
           </div>
         </div>
       </div>
@@ -259,7 +265,10 @@ export default function Portfolio() {
             >
               Sir David Clary’s Book
             </a>
-            ,{" "}<i className="text-sm">former President of Magdalen College, Oxford</i>
+            ,{" "}
+            <i className="text-sm">
+              former President of Magdalen College, Oxford
+            </i>
           </li>
           <li>
             Watch demo walkthrough:{" "}
