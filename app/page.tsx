8154e1d6214de2ff37b212b9906391e4a12463f8
John Seong <wonmor@gmail.com>
2025-08-07 22:09:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -371,7 +371,7 @@ export default function Portfolio() {
             3D Face Scan only using an iPhone
           </h4>
           <p className="mt-2 text-gray-300">
-            Portable, real‑time 3D face scanning app I developed for medical application.
+            Portable, real‑time 3D face scanning app I developed for medical (vision and dental) applications.
           </p>
         </div>
       </div>
